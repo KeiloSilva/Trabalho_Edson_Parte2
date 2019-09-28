ca6da8bff017511bc826e274d67ae5602ea21dbc
KeiloSilva <keilomarcio062014@gmail.com>
2019-09-28 16:12:13 -0300

    Trabalho Login

---
+++ CRUDpos/app/src/main/java/android/example/crudpos/activity/LogarActivity.java
@@ -0,0 +1,14 @@
+package android.example.crudpos.activity;
+
+import android.example.crudpos.R;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logar);
+    }
+}
